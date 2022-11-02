5619f7c9a84169aa0c0a6be202e0cf7f8d1a4dc7
RAFAEL FIGUEIREDO DA COSTA <8180246@estg.ipp.pt>
2022-11-02 10:48:17 +0000

    Estrutura do projeto

---
+++ app/src/main/java/com/example/mysmarthome/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.mysmarthome
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mysmarthome.ui.theme.MySmartHomeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MySmartHomeTheme {
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Text("MySmartHome")
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MySmartHomeTheme {
+
+    }
+}
