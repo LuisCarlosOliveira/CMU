d5e27b3bbc006c5578642c2f3321c57c1529aaf8
RAFAEL FIGUEIREDO DA COSTA <8180246@estg.ipp.pt>
2022-11-28 09:53:55 +0000

    Changes in NewAccountScreen for Phone

--- app/src/main/java/com/example/mysmarthome/ui/screens/phone/NewAccountScreen.kt
+++ app/src/main/java/com/example/mysmarthome/ui/screens/phone/NewAccountScreen.kt
@@ -1,19 +1,20 @@
 package com.example.mysmarthome.ui.screens.phone
 
 import android.R
-import androidx.compose.foundation.BorderStroke
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material.icons.rounded.Save
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -29,14 +30,19 @@ fun NewAccountScreen(/*navController: NavController*/) {
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
+
         val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
         val configuration = LocalConfiguration.current
         val screenWidth = configuration.screenWidthDp.dp
+        val localCtx= LocalContext.current
 
         var letterSpacing by remember {
             mutableStateOf(1.sp)
         }
         var nome by rememberSaveable {
+            mutableStateOf("")
+        }
+        var contacto by rememberSaveable {
             mutableStateOf("")
         }
         var email by rememberSaveable {
@@ -59,19 +65,19 @@ fun NewAccountScreen(/*navController: NavController*/) {
             scaffoldState = scaffoldState,
             topBar = {
                 Row(
-                    modifier = Modifier
+                    horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top = 20.dp)
+                        .padding(top = 10.dp)
                 ) {
-                    Button(
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
-                        modifier = Modifier
-                            .padding(start = 20.dp)
-                            .width(50.dp),
-                        onClick = {}) {
+                    IconButton(
+                        onClick = { }
+                    ) {
                         Icon(
                             Icons.Rounded.ArrowBack, "",
                             tint = Color.Black,
+                            modifier = Modifier
+                                .width(50.dp)
+                                .padding(start = 5.dp),
                         )
                     }
                     Text(
@@ -79,15 +85,31 @@ fun NewAccountScreen(/*navController: NavController*/) {
                         letterSpacing = letterSpacing,
                         fontFamily = FontFamily.Serif,
                         color = Color.Black,
-                        modifier = Modifier.padding(top = 7.dp, start = 20.dp),
+                        modifier = Modifier.padding(top = 7.dp),
                         fontSize = 24.sp,
                         text = "Criar Conta"
                     )
-
+                    IconButton(
+                        onClick = {
+                            if (nome.isNotEmpty() && contacto.isNotEmpty() && email.isNotEmpty() &&
+                                password.isNotEmpty() && password2.isNotEmpty()
+                            ) {
+                                Toast.makeText(localCtx,"Conta Criada!",Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    ) {
+                        Icon(
+                            Icons.Rounded.Save, "",
+                            tint = Color.Black,
+                            modifier = Modifier
+                                .width(50.dp)
+                                .padding(end = 10.dp),
+                        )
+                    }
                 }
                 Divider(
                     startIndent = 20.dp, thickness = 1.dp, color = Color.Black, modifier = Modifier
-                        .padding(top = 80.dp)
+                        .padding(top = 55.dp)
                         .width(screenWidth - 20.dp)
                 )
             },
@@ -104,13 +126,35 @@ fun NewAccountScreen(/*navController: NavController*/) {
                         disabledIndicatorColor = Color.Blue
                     ),
                         modifier = Modifier
-                            .padding(start = 20.dp, top = 20.dp, end = 20.dp)
+                            .padding(start = 20.dp, top = 10.dp, end = 20.dp)
                             .fillMaxWidth(),
                         value = nome,
                         shape = RoundedCornerShape(7.dp),
                         onValueChange = { nome = it },
                         placeholder = { Text(text = "Insira o Nome") },
                         label = { Text(text = "Nome") })
+                    Text(
+                        modifier = Modifier.padding(start = 20.dp, top = 20.dp),
+                        fontWeight = FontWeight.Bold,
+                        text = "Contacto Telefónico"
+                    )
+                    TextField(colors = TextFieldDefaults.textFieldColors(
+                        focusedIndicatorColor = Color.Blue,
+                        unfocusedIndicatorColor = Color.Blue,
+                        disabledIndicatorColor = Color.Blue
+                    ),
+                        modifier = Modifier
+                            .padding(start = 20.dp, top = 10.dp, end = 20.dp)
+                            .fillMaxWidth(),
+                        value = contacto,
+                        shape = RoundedCornerShape(7.dp),
+                        onValueChange = { contacto = it },
+                        placeholder = { Text(text = "Insira o Contacto") },
+                        label = { Text(text = "Contacto") },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        )
+                    )
 
                     Text(
                         modifier = Modifier.padding(start = 20.dp, top = 20.dp),
@@ -124,7 +168,7 @@ fun NewAccountScreen(/*navController: NavController*/) {
                         disabledIndicatorColor = Color.Blue
                     ),
                         modifier = Modifier
-                            .padding(start = 20.dp, top = 20.dp, end = 20.dp)
+                            .padding(start = 20.dp, top = 10.dp, end = 20.dp)
                             .fillMaxWidth(),
                         value = email,
                         shape = RoundedCornerShape(7.dp),
@@ -139,9 +183,9 @@ fun NewAccountScreen(/*navController: NavController*/) {
                     )
 
                     val icon = if (passwordVisibility)
-                        painterResource(id = R.drawable.ic_partial_secure)
+                        painterResource(id = android.R.drawable.ic_partial_secure)
                     else
-                        painterResource(id = R.drawable.ic_secure)
+                        painterResource(id = android.R.drawable.ic_secure)
 
                     TextField(
                         colors = TextFieldDefaults.textFieldColors(
@@ -150,7 +194,7 @@ fun NewAccountScreen(/*navController: NavController*/) {
                             disabledIndicatorColor = Color.Blue
                         ),
                         modifier = Modifier
-                            .padding(start = 20.dp, top = 20.dp, end = 20.dp)
+                            .padding(start = 20.dp, top = 10.dp, end = 20.dp)
                             .fillMaxWidth(),
                         value = password,
                         onValueChange = { password = it },
@@ -165,7 +209,6 @@ fun NewAccountScreen(/*navController: NavController*/) {
                         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                         visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation()
                     )
-
                     Text(
                         modifier = Modifier.padding(start = 20.dp, top = 20.dp),
                         fontWeight = FontWeight.Bold,
@@ -173,9 +216,9 @@ fun NewAccountScreen(/*navController: NavController*/) {
                     )
 
                     val icon2 = if (passwordVisibility2)
-                        painterResource(id = R.drawable.ic_partial_secure)
+                        painterResource(id = android.R.drawable.ic_partial_secure)
                     else
-                        painterResource(id = R.drawable.ic_secure)
+                        painterResource(id = android.R.drawable.ic_secure)
 
                     TextField(
                         colors = TextFieldDefaults.textFieldColors(
@@ -184,35 +227,21 @@ fun NewAccountScreen(/*navController: NavController*/) {
                             disabledIndicatorColor = Color.Blue
                         ),
                         modifier = Modifier
-                            .padding(start = 20.dp, top = 20.dp, end = 20.dp)
+                            .padding(start = 20.dp, top = 10.dp, end = 20.dp, bottom = 20.dp)
                             .fillMaxWidth(),
                         value = password2,
                         onValueChange = { password2 = it },
                         shape = RoundedCornerShape(7.dp),
-                        placeholder = { Text(text = "Repetir Nova Password") },
+                        placeholder = { Text(text = "Repetir Password") },
                         label = { Text(text = "Repetir Password") },
                         trailingIcon = {
                             IconButton(onClick = { passwordVisibility2 = !passwordVisibility2 }) {
-                                Icon(painter = icon2, contentDescription = "")
+                                Icon(painter = icon, contentDescription = "")
                             }
                         },
                         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                         visualTransformation = if (passwordVisibility2) VisualTransformation.None else PasswordVisualTransformation()
                     )
-
-                    Button(
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray),
-                        border = BorderStroke(1.dp, Color.Blue),
-                        modifier = Modifier
-                            .padding(start = 130.dp, top = 20.dp, bottom = 20.dp),
-                        shape = RoundedCornerShape(20.dp),
-                        onClick = {}) {
-                        Text(fontWeight = FontWeight.Medium, text = "Continuar")
-                        Icon(
-                            Icons.Rounded.ArrowForward, "",
-                            tint = Color.Black,
-                        )
-                    }
                 }
             }
         )
