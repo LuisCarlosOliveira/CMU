5db25a05129c5337a74742d52d6c6382c90d0904
RAFAEL FIGUEIREDO DA COSTA <8180246@estg.ipp.pt>
2022-12-01 18:07:21 +0000

    Retrofit API Calls Continues

--- app/src/main/java/com/example/mysmarthome/retrofit/shelly_api/blind/BlindAPI.kt
+++ app/src/main/java/com/example/mysmarthome/retrofit/shelly_api/blind/BlindAPI.kt
@@ -1,5 +1,39 @@
 package com.example.mysmarthome.retrofit.shelly_api.blind
 
+import com.example.mysmarthome.retrofit.data_models.blind.BlindMeters
+import com.example.mysmarthome.retrofit.data_models.blind.BlindRoller
+import com.example.mysmarthome.retrofit.data_models.light.Light
+import com.example.mysmarthome.retrofit.data_models.light.LightMeters
+import com.example.mysmarthome.retrofit.data_models.light.LightsData
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface BlindAPI {
 
+    @GET("/roller/0")
+    suspend fun getBlindRoller(
+
+        @Query("state") state: String,
+        @Query("power") power: Float,
+        @Query("is_valid") is_valid: Boolean,
+        @Query("overtemperature") overtemperature: Boolean,
+        @Query("stop_reason") stop_reason: String,
+        @Query("last_direction") last_direction: String,
+        @Query("current_pos") current_pos: Int,
+        @Query("calibrating") calibrating: Boolean,
+        @Query("positioning") positioning: Boolean
+
+    ): Response<BlindRoller>
+
+
+    @GET("/meter/:meter_id")
+    suspend fun getLightMeter(
+
+        @Query("power") power: Float,
+        @Query("overpower") overpower: Float,
+        @Query("is_valid") is_valid: Boolean,
+        @Query("total") total: Float,
+
+    ): Response<BlindMeters>
 }
--- app/src/main/java/com/example/mysmarthome/retrofit/shelly_api/light/LightAPI.kt
+++ app/src/main/java/com/example/mysmarthome/retrofit/shelly_api/light/LightAPI.kt
@@ -1,4 +1,34 @@
 package com.example.mysmarthome.retrofit.shelly_api.light
 
+import com.example.mysmarthome.retrofit.data_models.light.Light
+import com.example.mysmarthome.retrofit.data_models.light.LightMeters
+import com.example.mysmarthome.retrofit.data_models.light.LightsData
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface LightAPI {
+
+    @GET("/light/0")
+    suspend fun getLight(
+
+        @Query("wifiConnection") wifiConnection: Boolean,
+        @Query("lightID") lightID: String,
+        @Query("lightIP") lightIP: String,
+        @Query("lights") lights: List<LightsData>,
+        @Query("meters") meters: List<LightMeters>,
+        @Query("temperature") temperature: Float,
+        @Query("overTemperature") overTemperature: Boolean
+
+    ) : Response<Light>
+
+    @GET("/meter/:meter_id")
+    suspend fun getLightMeter(
+
+        @Query("power") power: Float ,
+        @Query("is_valid") is_valid: Boolean ,
+        @Query("total") total: Float ,
+
+    ) : Response<LightMeters>
+
 }
