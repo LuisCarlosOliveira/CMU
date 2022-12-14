f9067cb2b8d6f81821db730386f9fe1008a2927b
RAFAEL FIGUEIREDO DA COSTA <8180246@estg.ipp.pt>
2022-12-14 15:41:34 +0000

    Lists and Popups

---
+++ app/src/main/java/com/example/mysmarthome/ui/components/Lists.kt
@@ -0,0 +1,90 @@
+package com.example.mysmarthome.ui.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ListRowWithCheckbox(title: String) {
+
+    var letterSpacing by remember {
+        mutableStateOf(1.sp)
+    }
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .padding(bottom = 20.dp, start = 20.dp, end = 20.dp)
+            .border(
+                border =
+                BorderStroke(width = 1.dp, Color.LightGray)
+            )
+    ) {
+        Text(
+            fontWeight = FontWeight.Medium,
+            letterSpacing = letterSpacing,
+            fontFamily = FontFamily.Serif,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(top = 15.dp, start = 20.dp),
+            fontSize = 18.sp,
+            text = title
+        )
+        CheckBoxButton()
+    }
+}
+
+@Composable
+fun ListRowWithIcon(title: String, icon: ImageVector) {
+
+    var letterSpacing by remember {
+        mutableStateOf(1.sp)
+    }
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .padding(bottom = 20.dp, start = 20.dp, end = 20.dp)
+            .border(
+                border =
+                BorderStroke(width = 1.dp, Color.LightGray)
+            )
+    ) {
+        Text(
+            fontWeight = FontWeight.Medium,
+            letterSpacing = letterSpacing,
+            fontFamily = FontFamily.Serif,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(top = 15.dp, start = 20.dp),
+            fontSize = 18.sp,
+            text = title
+        )
+        IconButton(
+            onClick = {  }
+        ) {
+            Icon(
+                icon, "",
+                tint = Color.Black,
+                modifier = Modifier
+                    .width(50.dp)
+                    .padding(top = 15.dp, end = 20.dp),
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mysmarthome/ui/components/Popups.kt
@@ -0,0 +1,155 @@
+package com.example.mysmarthome.ui.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
+
+@Composable
+fun AlertPopup(
+    state: Boolean,
+    btn1Text: String,
+    btn2Text: String,
+    contentText: String,
+    actionBtn: () -> Unit,
+    actionBtnExit: () -> Unit
+) {
+    var dialogOpen by remember { mutableStateOf(false) }
+    dialogOpen = state
+    if (dialogOpen) {
+        AlertDialog(
+            onDismissRequest = { dialogOpen = false },
+            buttons = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 5.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Button(colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Blue
+                    ),
+                        onClick = {
+                            actionBtn()
+                        }) {
+                        Text(
+                            color = Color.White,
+                            text = btn1Text
+                        )
+                    }
+                    Button(colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Blue
+                    ),
+                        onClick = { actionBtnExit() }) {
+                        Text(
+                            color = Color.White,
+                            text = btn2Text
+                        )
+                    }
+                }
+            },
+
+            title = { },
+
+            text = {
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                ) {
+                    Text(
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif,
+                        fontSize = 20.sp,
+                        color = Color.Black,
+                        text = contentText
+                    )
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(2.dp),
+            shape = RoundedCornerShape(10.dp),
+            backgroundColor = Color.White,
+            properties = DialogProperties(
+                dismissOnBackPress = true,
+                dismissOnClickOutside = true
+            )
+        )
+    }
+}
+
+/*
+@Composable
+fun InsertPopup(state:Boolean,btn1Text:String, btn2Text:String, actionBtn:()->Unit, actionBtn2:()->Unit,content: @Composable()()->Unit){
+    var dialogOpen by remember { mutableStateOf(false) }
+    dialogOpen = state
+    if (dialogOpen) {
+        AlertDialog(
+            onDismissRequest = { dialogOpen = false },
+            buttons = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 5.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Button(colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Blue
+                    ),
+                        onClick = {
+                            actionBtn()
+                        }) {
+                        Text(
+                            color = Color.White,
+                            text = btn1Text
+                        )
+                    }
+                    Button(colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Blue
+                    ),
+                        onClick = { actionBtn2() }) {
+                        Text(
+                            color = Color.White,
+                            text = btn2Text
+                        )
+                    }
+                }
+            },
+
+            title = { },
+
+            text = {
+                content()
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(2.dp),
+            shape = RoundedCornerShape(10.dp),
+            backgroundColor = Color.White,
+            properties = DialogProperties(
+                dismissOnBackPress = true,
+                dismissOnClickOutside = true
+            )
+        )
+    }
+}
+
+@Composable
+fun T111(){
+    FormNumberTextField(title = "" , placeholder = "", label ="" )
+}
+
+*/
