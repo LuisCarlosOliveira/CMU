1a3d5e236b6e5aff2fc5fe9a5e3024b3646073a7
Teresa Peixoto <8190334@estg,ipp,pt>
2022-12-18 19:23:27 +0000

    retrofit

--- app/src/main/java/com/example/mysmarthome/retrofit/shelly_api/light/LightAPI.kt
+++ app/src/main/java/com/example/mysmarthome/retrofit/shelly_api/light/LightAPI.kt
@@ -3,6 +3,7 @@ package com.example.mysmarthome.retrofit.shelly_api.light
 import com.example.mysmarthome.retrofit.data_models.light.Light
 import com.example.mysmarthome.retrofit.data_models.light.LightMeters
 import com.example.mysmarthome.retrofit.data_models.light.LightsData
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,25 +11,9 @@ import retrofit2.http.Query
 interface LightAPI {
 
     @GET("/light/0")
-    suspend fun getLight(
-
-        @Query("wifiConnection") wifiConnection: Boolean,
-        @Query("lightID") lightID: String,
-        @Query("lightIP") lightIP: String,
-        @Query("lights") lights: List<LightsData>,
-        @Query("meters") meters: List<LightMeters>,
-        @Query("temperature") temperature: Float,
-        @Query("overTemperature") overTemperature: Boolean
-
-    ) : Response<Light>
+    suspend fun getLight() : Call<Light>
 
     @GET("/meter/:meter_id")
-    suspend fun getLightMeter(
-
-        @Query("power") power: Float ,
-        @Query("is_valid") is_valid: Boolean ,
-        @Query("total") total: Float ,
-
-    ) : Response<LightMeters>
+    suspend fun getLightMeter() : Call<LightMeters>
 
 }
