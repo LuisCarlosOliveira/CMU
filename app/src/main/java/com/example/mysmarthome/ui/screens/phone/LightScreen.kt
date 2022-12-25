dcd05ed1b5e8d96fba40fbb1ea11554b4d010e62
RAFAEL FIGUEIREDO DA COSTA <8180246@estg.ipp.pt>
2022-12-25 14:55:42 +0000

    BlindScreen ready

--- app/src/main/java/com/example/mysmarthome/ui/screens/phone/LightScreen.kt
+++ app/src/main/java/com/example/mysmarthome/ui/screens/phone/LightScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
