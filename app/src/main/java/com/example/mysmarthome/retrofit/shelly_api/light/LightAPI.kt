906671023389fd651bea374341454c85979f0f49
Teresa Peixoto <8190334@estg.ipp.pt>
2022-12-27 15:01:14 +0000

    pequena atualizacao

--- app/src/main/java/com/example/mysmarthome/retrofit/shelly_api/light/LightAPI.kt
+++ app/src/main/java/com/example/mysmarthome/retrofit/shelly_api/light/LightAPI.kt
@@ -2,11 +2,8 @@ package com.example.mysmarthome.retrofit.shelly_api.light
 
 import com.example.mysmarthome.retrofit.data_models.light.Light
 import com.example.mysmarthome.retrofit.data_models.light.LightMeters
-import com.example.mysmarthome.retrofit.data_models.light.LightsData
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface LightAPI {
 
@@ -15,5 +12,4 @@ interface LightAPI {
 
     @GET("/meter/:meter_id")
     suspend fun getLightMeter() : Call<LightMeters>
-
 }
--- app/src/main/java/com/example/mysmarthome/retrofit/shelly_api/plug/PlugAPI.kt
+++ app/src/main/java/com/example/mysmarthome/retrofit/shelly_api/plug/PlugAPI.kt
@@ -1,38 +1,17 @@
 package com.example.mysmarthome.retrofit.shelly_api.plug
 
-import com.example.mysmarthome.retrofit.data_models.light.Light
-import com.example.mysmarthome.retrofit.data_models.light.LightMeters
-import com.example.mysmarthome.retrofit.data_models.light.LightsData
 import com.example.mysmarthome.retrofit.data_models.plug.Plug
 import com.example.mysmarthome.retrofit.data_models.plug.PlugMeters
-import com.example.mysmarthome.retrofit.data_models.plug.PlugRelays
-import retrofit2.Response
+import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface PlugAPI {
 
     @GET("/relay/0")
-    suspend fun getPlugRelay(
-
-        @Query("wifiConnection") wifiConnection: Boolean,
-        @Query("plugID") plugID: String,
-        @Query("plugIP") plugIP: String,
-        @Query("relays") relays: List<PlugRelays>,
-        @Query("meters") meters: List<PlugMeters>,
-        @Query("temperature") temperature: Float,
-        @Query("overtemperature") overtemperature: Boolean
-
-    ) : Response<Plug>
+    fun getPlugRelay() : Call<Plug>
 
 
     @GET("/meter/:meter_id")
-    suspend fun getPlugMeter(
-
-        @Query("power") power: Float,
-        @Query("is_valid") is_valid: Boolean,
-        @Query("total") total: Float,
-
-    ) : Response<PlugMeters>
+    suspend fun getPlugMeter() : Call<PlugMeters>
 
 }
