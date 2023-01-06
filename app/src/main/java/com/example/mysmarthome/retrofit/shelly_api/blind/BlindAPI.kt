5cf30043506632e36e60a50ef440313a78925797
RAFAEL FIGUEIREDO DA COSTA <8180246@estg.ipp.pt>
2023-01-06 10:51:46 +0000

    Helper and BlindApi

--- app/src/main/java/com/example/mysmarthome/retrofit/shelly_api/blind/BlindAPI.kt
+++ app/src/main/java/com/example/mysmarthome/retrofit/shelly_api/blind/BlindAPI.kt
@@ -1,15 +1,26 @@
 package com.example.mysmarthome.retrofit.shelly_api.blind
 
+import com.example.mysmarthome.retrofit.data_models.blind.Blind
 import com.example.mysmarthome.retrofit.data_models.blind.BlindMeters
-import com.example.mysmarthome.retrofit.data_models.blind.BlindRoller
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface BlindAPI {
 
     @GET("/roller/0")
-    fun getBlindRoller(): Call<BlindRoller>
+    suspend fun getBlind(): Response<Blind>
 
-    @GET("/meter/:meter_id")
-    suspend fun getLightMeter(): Call<BlindMeters>
+    @GET("/roller/0?")
+    suspend fun getBlindActions(
+        @Query("go") go: String,
+        @Query("roller_pos") roller_pos: Int,
+    ): Response<Blind>
+
+    @GET("/meter/{meter_id}")
+    suspend fun getBlindMeter(
+        @Path("meter_id") meter_id: Int
+    ): Response<BlindMeters>
+
 }
